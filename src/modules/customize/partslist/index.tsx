a6a46cc943d8151d20cb358b58c0d4f241141482
Blank <iddinishak@gmail.com>
2023-05-24 17:50:08 +0800

    cust2

--- src/modules/customize/partslist/index.tsx
+++ src/modules/customize/partslist/index.tsx
@@ -1,11 +1,25 @@
 import { partsType, PartId } from "@modules/buildpc/partlist"
-
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { medusaClient } from "@lib/config"
+import { useCart } from "medusa-react"
+import { partList } from "@modules/buildpc/partlist"
 interface PartSelectorProps {
   parts: partsType[]
   selectedPart: PartId
 }
 
 const PartsList: React.FC<PartSelectorProps> = ({ selectedPart, parts }) => {
+  const { cart } = useCart()
+
+  const fetchCollections = async (id: string) => {
+    const { products, count, offset } = await medusaClient.products.list({
+      limit: 12,
+      offset: 0, //change if paged
+      collection_id: [id],
+      cart_id: cart?.id,
+    })
+  }
+
   return (
     <>
       <div>PartsList</div>
