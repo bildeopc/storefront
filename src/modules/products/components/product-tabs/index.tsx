ab9f0da7e9cda1fd2b90d68878da1b864e7acf39
Blank <iddinishak@gmail.com>
2023-07-07 04:55:20 +0800

    hidden products details for now

--- src/modules/products/components/product-tabs/index.tsx
+++ src/modules/products/components/product-tabs/index.tsx
@@ -61,20 +61,20 @@ const ProductInfoTab = ({ product }: ProductTabsProps) => {
     <Tab.Panel className="text-small-regular py-8">
       <div className="grid grid-cols-2 gap-x-8">
         <div className="flex flex-col gap-y-4">
-          <div>
+          {/* <div>
             <span className="font-semibold">Material</span>
             <p>{product.material ? product.material : "-"}</p>
-          </div>
-          <div>
+          </div> */}
+          {/* <div>
             <span className="font-semibold">Country of origin</span>
             <p>{product.origin_country ? product.origin_country : "-"}</p>
-          </div>
+          </div> */}
           <div>
             <span className="font-semibold">Type</span>
             <p>{product.type ? product.type.value : "-"}</p>
           </div>
         </div>
-        <div className="flex flex-col gap-y-4">
+        {/* <div className="flex flex-col gap-y-4">
           <div>
             <span className="font-semibold">Weight</span>
             <p>{product.weight ? `${product.weight} g` : "-"}</p>
@@ -87,7 +87,7 @@ const ProductInfoTab = ({ product }: ProductTabsProps) => {
                 : "-"}
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
       {product.tags.length ? (
         <div>
